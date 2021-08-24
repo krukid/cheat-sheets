141f66af4a36e79bce5d24ad5474a5dfd81ffb25
Viktors Buls <krukid@gmail.com>
2021-08-24 19:32:53 +0300

    Create bench.sh

---
+++ bash/bench.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+data() {
+	local ts="$(date +%s)_$1"
+	local payload
+	read -r -d '' payload <<- EOM
+		{
+			"some_key": "some_value",
+			"some_seed": "seed_${ts}",
+			"some_array": ["lol", "kek"]
+		}
+	EOM
+	echo $payload
+}
+
+for j in {1..3}; do # serial batches
+	PIDS=()
+	for i in {1..3}; do # parallel jobs
+		time curl -d "$(data $i)" -H "content-type: application/json" "http://localhost:3009/api" &
+		PIDS+=($!)
+	done
+	wait $PIDS
+done
